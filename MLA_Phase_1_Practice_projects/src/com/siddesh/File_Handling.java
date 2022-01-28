e6477783e8d77d5fbea6da74934a1d984b85ad49
Siddeshahp <siddeshsid99@gmail.com>
2022-01-28 15:22:46 +0530

    commit done

---
+++ MLA_Phase_1_Practice_projects/src/com/siddesh/File_Handling.java
@@ -0,0 +1,35 @@
+package com.siddesh;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class File_Handling 
+{
+	public static void main( String[] args ) 
+	  {
+
+	      try 
+	      {
+	         String data = " Hi, How are you.";
+	         File f1 = new File("D:\\siddesh");
+	         if(!f1.exists()) 
+	         {
+	            f1.createNewFile();
+	         }
+
+	         FileWriter fileWritter = new FileWriter(f1.getName(),true);
+	         BufferedWriter bw = new BufferedWriter(fileWritter);
+	         bw.write(data);
+	         bw.close();
+	         System.out.println("Done");
+	       } 
+	      
+	      catch(IOException e)
+	      {
+	         e.printStackTrace();
+	      }
+	   }
+
+}
